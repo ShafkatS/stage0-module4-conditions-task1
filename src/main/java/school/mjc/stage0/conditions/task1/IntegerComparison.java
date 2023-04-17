4d81550d6a2a55a60875621e12ddecf3f04f39db
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-04-17 21:02:29 +0500

    Update IntegerComparison.java

--- src/main/java/school/mjc/stage0/conditions/task1/IntegerComparison.java
+++ src/main/java/school/mjc/stage0/conditions/task1/IntegerComparison.java
@@ -1,7 +1,6 @@
 package school.mjc.stage0.conditions.task1;
 
 public class IntegerComparison {
-    public static void main(String[] args) {
         if (true) {
             System.out.println(true);
         }
