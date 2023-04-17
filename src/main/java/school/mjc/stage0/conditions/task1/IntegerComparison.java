924a8d2ab64ff0549cf21e8b93a2321332b2c95d
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-04-17 20:58:18 +0500

    Update IntegerComparison.java

--- src/main/java/school/mjc/stage0/conditions/task1/IntegerComparison.java
+++ src/main/java/school/mjc/stage0/conditions/task1/IntegerComparison.java
@@ -2,10 +2,9 @@ package school.mjc.stage0.conditions.task1;
 
 public class IntegerComparison {
     public static void main(String[] args) {
-        int x = 50;
-        int y = 50;
-        __ (x __ y) {
-            System.out.println("Hello, World!");
+        public static void main(String[] args) {
+        if (true) {
+            System.out.println(true);
         }
     }
 }
